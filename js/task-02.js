42fb4cd5407e135c396c268e076a08bdcfc6fe81
Andron1976 <and.ron@ukr.net>
2022-08-19 16:47:45 +0300

    did task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// ingredients.forEach((ingredient) => {
+//   listItem.textContent = ingredient;
+// });
+const unnumberedList = document.querySelector('#ingredients');
+
+const ingredientsEl = ingredients.map((ingredient) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('item');
+  listItem.textContent = ingredient;
+  return listItem;
+});
+unnumberedList.append(...ingredientsEl);
+console.log(ingredientsEl);
