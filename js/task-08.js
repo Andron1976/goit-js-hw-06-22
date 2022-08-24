cf3642a8fbef4c6a187c96baedad7fcd174264f2
Andron1976 <and.ron@ukr.net>
2022-08-24 14:23:34 +0300

    did task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formSending = document.querySelector('.login-form');
 
+formSending.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+    event.preventDefault();
+    const {
+        elements: {email, password}} = event.currentTarget;
+ if (email.value === '' || password.value === ''){
+    return alert('Please, Fill in all the fields')
+ }
+
+ const formElements = event.currentTarget.elements;
+ const emailInput = formElements.email.value;
+ const passwordInput = formElements.password.value;
+
+ const formData = {
+    emailInput,
+    passwordInput,
+ }
+ console.log(formData);
+
+  event.currentTarget.reset();
+
+}
