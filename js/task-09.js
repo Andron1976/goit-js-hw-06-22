02e6545eb53fbf19a4ec02438d635290a0e2f719
Andron1976 <and.ron@ukr.net>
2022-08-24 15:14:04 +0300

    did task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const handleButton = document.querySelector(".change-color");
+const fillSpan = document.querySelector('.color');
+const changeColorBody = document.querySelector('body');
+
+handleButton.addEventListener('click', () => {
+  changeColorBody.style.backgroundColor = getRandomHexColor();
+  fillSpan.textContent = getRandomHexColor();
+})
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+console.log(getRandomHexColor());
